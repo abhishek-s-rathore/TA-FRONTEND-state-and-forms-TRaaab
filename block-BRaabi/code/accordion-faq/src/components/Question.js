d7aaf9313f9c3c521c0b50bcb4a58b6f90299abe
abhishek-s-rathore <abhishek.990914@gmail.com>
2021-10-20 21:00:03 +0530

    FAQs completed

---
+++ block-BRaabi/code/accordion-faq/src/components/Question.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class Question extends React.Component{
+   constructor(props){
+       super(props);
+       this.state = {
+              active : null,
+       };
+   }
+
+   handleClick = (incId)=>{
+             this.setState({
+                 active : this.state.active === incId ? null : incId,
+             })
+       }
+
+
+   render(){
+
+       return(
+           <>
+           <article className="question">
+               <div className={this.state.active === this.props.index ? 'questionbox active' : 'questionbox'}>
+               <h1>{this.props.ques.Q}</h1> 
+               <p>
+               <span onClick={()=> this.handleClick(this.props.index)}>
+                   <i class="fas fa-hand-point-down"  className={this.state.active === this.props.index ? 'fas fa-hand-point-up' : 'fas fa-hand-point-down'}></i> </span>
+               </p>
+               </div>
+              
+              {
+                this.state.active === this.props.index ?   <> <p className="answerbox"> {this.props.ques.A}</p>  </>: ''
+               }
+           </article>
+           </>
+       );
+
+   }
+}
+
+
+export default Question;
+
